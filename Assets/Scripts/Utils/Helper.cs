2d796702486fdf0c373c97febdbf66e42176e247
Bangeunseong <bangeunseong@naver.com>
2025-06-05 12:20:06 +0900

    [feat] First Commit, Added Util Files

---
+++ Assets/Scripts/Utils/Helper.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public static class Helper
+    {
+        public static T GetComponent_Helper<T>(this GameObject go) where T : Component
+        {
+            if(!go) { Debug.LogError("GameObject is null!"); return null; }
+
+            if (go.TryGetComponent(out T component)) return component;
+            Debug.LogError($"{typeof(T)} is not found in {go.name}!"); 
+            return null;
+        }
+
+        public static T GetComponentInChildren_Helper<T>(this GameObject go, bool includeInActive = false) where T : Component
+        {
+            if(!go) { Debug.LogError("GameObject is null!"); return null; }
+            var component = go.GetComponentInChildren<T>(includeInActive);
+            if(component) return component;
+            Debug.LogError($"{typeof(T)} is not found in {go.name} and children of {go.name}!"); 
+            return null;
+        }
+
+        public static T GetComponentInParent_Helper<T>(this GameObject go, bool includeInActive = false) where T : Component
+        {
+            if(!go) { Debug.LogError("GameObject is null!"); return null; }
+            var component = go.GetComponentInParent<T>(includeInActive);
+            if(component) return component;
+            Debug.LogError($"{typeof(T)} is not found in {go.name} and parents of {go.name}!"); 
+            return null;
+        }
+    }
+}
